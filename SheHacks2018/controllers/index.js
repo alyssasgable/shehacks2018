2a8d9ac8260898fe8c9499faf5be707dff2ed11b
Unknown <alyssasgable@gmail.com>
2018-01-28 02:32:22 -0500

    added firebase integration

--- SheHacks2018/controllers/index.js
+++ SheHacks2018/controllers/index.js
@@ -7,10 +7,23 @@ module.exports = function (router) {
 
     var model = new IndexModel();
 
-    router.get('/', function (req, res) {
-        
-        res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
-        
-    });
+    // router.get('/', function (req, res) {
+    //
+    //     // res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
+    //     res.send('<h1>Hello WOrld</h1>');
+    //
+    // });
+    var express = require('express'),
+    enrouten = require('express-enrouten');
+
+    var app = express();
+
+    app.use(enrouten({
+      routes: [ -
+         { path: '/',    method: 'GET', handler: require(/Users/alyssa/github/shehacks2018/SheHacks2018/view/login.html) },
+         // { path: '/foo', method: 'GET', handler: require('./routes/foo') },
+         // { path: '/admin', method: 'GET', handler: require('./routes/foo'), middleware: [isAuthenticated] }
+     ]
+}));
 
 };
